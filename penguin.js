e6f53082644ddd7c8efc364f560b0eb375d29c41
NURAH <Nurah.Muhammad@detroitLabs.com>
2017-03-24 14:16:26 -0400

    created a function for user to enter their nam and name their penguin

--- penguin.js
+++ penguin.js
@@ -1,2 +1,22 @@
 var sget = require("sget");
 var Penguins = [];
+
+var mainMenu = function() {
+  // wipeScreen();
+  
+  sleep(400);
+  printMenu();
+  sleep(400);
+
+
+    var start = function (){
+var name = sget ("\n Please enter your name: \n").trim();
+userName = name;
+console.log ("\nWelcome "  + userName + "!");
+var nameZoo = sget ("\n Please enter the name of your penguin: \n").trim ();
+penguinName = namePenguin;
+console.log ("\nYour penguin name is: " + penguinName + "\n\n");
+mainMenu();
+
+};
+};
